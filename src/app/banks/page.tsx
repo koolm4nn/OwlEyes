202988b19c08932551916a425334efc03cd1897c
koolm4nn <mar_kuhlmann@protonmail.com>
2025-08-09 09:10:22 +1200

    Removed the bank list by table

--- src/app/banks/page.tsx
+++ src/app/banks/page.tsx
@@ -1,17 +1,23 @@
 "use client";
 
 import { AddBankForm } from "@/components/AddBankForm";
-import { BankList } from "@/components/BankList";
+import { BanksTable } from "@/components/Tables/BanksTable";
+import { useBanks } from "@/hooks/useBank";
 
 /**
  * Page for banks
  */
 
 export default function Banks(){
+    const {data: banks = [], isLoading, error } = useBanks();
+
+    if(isLoading) return <p>Is Loading...</p>;
+    if(error) return <p>Error Loading Banks</p>
+
     return (
         <div className="flex gap-8 p-6">
             <div className="w=1/2 border-r pr-4">
-                <BankList />
+                <BanksTable banks={banks} />
             </div>
             <div className="w=1/2 pl-4">
                 <AddBankForm />
