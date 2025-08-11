75fd944613446ba36bcfadeaecd59b88e4b4d025
koolm4nn <mar_kuhlmann@protonmail.com>
2025-08-11 23:18:06 +1200

    Add Table and Form to "Accounts" page [skip ci]

--- src/app/accounts/page.tsx
+++ src/app/accounts/page.tsx
@@ -1,7 +1,29 @@
+"use client"
+
+import { AddAccountForm } from "@/components/Forms/AddAccountForm";
+import { AccountsTable } from "@/components/Tables/AccountsTable/AccountsTable";
+import { useAccounts } from "@/hooks/useAccount";
+
 /*
  * Page for the accounts 
  */
 
-export default function A(){
-    return <h1>Accounts</h1>
+export default function Accounts(){
+    const {data: accounts = [], isLoading, error } = useAccounts();
+
+    if(isLoading) return <p>Is Loading...</p>;
+    if(error) return <p>Error Loading Accounts</p>
+
+    return (
+        <div className="flex justify-center px-12 py-8">
+            <div className="flex w-full max-w-6xl gap-8">
+                <div className="basis-[60%] border-r pr-6">
+                    <AccountsTable accounts={accounts} />
+                </div>
+                <div className="basis-[40%] pl-6">
+                    <AddAccountForm />
+                </div>
+            </div>
+        </div>
+    )
 }
