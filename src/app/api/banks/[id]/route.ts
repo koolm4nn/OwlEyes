78f6f2a768b56dea57844a3c59d0673451d52fb2
koolm4nn <mar_kuhlmann@protonmail.com>
2025-08-21 21:42:54 +1200

    Add api for bank by id

---
+++ src/app/api/banks/[id]/route.ts
@@ -0,0 +1,38 @@
+import { NextRequest, NextResponse } from "next/server";
+import ServiceContainer from "@/services/serviceContainer";
+
+/**
+ * Api route handler for `/api/banks/with-accounts`.
+ * 
+ * Handles HTTP requests related to banks and their related accounts.
+ * 
+ */
+
+// Service layer instance
+const bankService = ServiceContainer.bankService;
+
+/**
+ * GET /api/banks/[id]
+ * 
+ * Deletes a bank.
+ * 
+ * @returns {NextResponse} 
+ *      200 OK with boolean about success of deletion
+ *      500 on server error
+ */
+export async function DELETE(req: Request, context: { params : Promise<{ id: string }> }) {
+    const { id } = await context.params; 
+
+    try{
+        const bankId = parseInt(id, 10);
+
+        if (isNaN(bankId)) {
+            return NextResponse.json({ error: "Invalid bank ID to delete" }, { status: 400 });
+        }
+
+        const result = bankService.delete(bankId);
+        return NextResponse.json({ success: result });
+    } catch(err){
+        return NextResponse.json({error: "Internal server error: failed to delete bank."}, {status: 500});
+    }
+}
